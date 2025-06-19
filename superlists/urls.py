154e2c2b81c293e10a9c28f27c70e93f81bd0dd4
Miqueias Penha <miqueiaspenha@gmail.com>
2025-06-19 20:38:37 -0300

    new URL, view and template to display lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from lists import views
 
-urlpatterns = [path("", views.home_page, name="home")]
+urlpatterns = [
+    re_path(r"^$", views.home_page, name="home"),
+    re_path(r"^lists/the-only-list-in-the-world/$", views.view_list, name="view_list"),
+]
